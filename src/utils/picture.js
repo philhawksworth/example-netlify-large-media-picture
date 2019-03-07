5dbafb8b0275aab45e4f2c250cd4275e27472d78
Phil Hawksworth <phil@netlify.com>
2019-03-07 16:02:50 +0000

    fix alt typo

--- src/utils/picture.js
+++ src/utils/picture.js
@@ -4,7 +4,7 @@ module.exports = (url, alt = "Missing alt text") => {
   return `<picture>
   <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
   <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
-  <img src="/images/${url}?nf_resize=fit&w=500" ast="${alt}" />
+  <img src="/images/${url}?nf_resize=fit&w=500" alt="${alt}" />
 </picture>`;
 };
 
