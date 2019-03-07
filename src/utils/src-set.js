ad8c800d515c982de470367133db4640cd709c32
Phil Hawksworth <phil@netlify.com>
2019-03-07 12:17:08 +0000

    move to picture element

--- src/utils/src-set.js
+++ src/utils/src-set.js
@@ -1,13 +1,11 @@
 // Generate an image tag using srcset for Netlify Large Media urls with image transforms
 
 module.exports = (url, alt = "Missing alt text") => {
-  return `<img srcset="/images/${url}?nf_resize=fit&w=400 400w,
-                       /images/${url}?nf_resize=fit&w=600 600w,
-                       /images/${url}?nf_resize=fit&w=700 700w"
-              sizes="(max-width: 740px) 600px,
-                     (max-width: 1200px) 700px,
-                      1000px"
-              src="/images/${url}?nf_resize=fit&w=1000"
-              alt="${alt}">`;
+  return `<picture>
+            <source srcset="/images/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
+            <source srcset="/images/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
+            <img src="/images/${url}?nf_resize=fit&w=500" ast="${alt}" />
+          </picture>`;s
 };
+
 
